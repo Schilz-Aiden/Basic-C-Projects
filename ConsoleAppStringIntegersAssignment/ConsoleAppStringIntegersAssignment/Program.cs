9faefd8109802df32e71cdafb996e2080dd3733f
Schilz-Aiden <aidenschilz5@gmail.com>
2025-08-05 11:57:21 -0600

    Add files via upload

---
+++ ConsoleAppStringIntegersAssignment/ConsoleAppStringIntegersAssignment/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStringIntegersAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Make a list of numbers
+            List<int> myNumbers = new List<int>() { 10, 20, 30, 40, 50 };
+
+            // Ask the user to type a number to divide by
+            Console.WriteLine("Hey! Enter a number to divide each number in the list by:");
+
+            try
+            {
+                // Read what the user types and turn it into an integer
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+
+                // Go through each number in the list
+                foreach (int number in myNumbers)
+                {
+                    // Divide the number by what the user entered and show the result
+                    int answer = number / userNumber;
+                    Console.WriteLine(number + " divided by " + userNumber + " is " + answer);
+                }
+            }
+            catch (FormatException)
+            {
+                // This runs if the user didn't type a number
+                Console.WriteLine("Oops! That wasn't a number. Please try again.");
+            }
+            catch (DivideByZeroException)
+            {
+                // This runs if the user typed 0
+                Console.WriteLine("Uh oh! You can't divide by zero!");
+            }
+            catch (Exception e)
+            {
+                // This catches any other unexpected errors
+                Console.WriteLine("Something went wrong: " + e.Message);
+            }
+
+            // Let the user know the program kept running even after an error
+            Console.WriteLine("The program kept going after the try/catch block. :)");
+
+            // Wait for the user to press a key before closing the window
+            Console.WriteLine("Press any key to close the program.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleAppStringIntegersAssignment/ConsoleAppStringIntegersAssignment/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleAppStringIntegersAssignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleAppStringIntegersAssignment")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("beb0235f-c27c-45a0-aefd-dda8bcedaaf1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
