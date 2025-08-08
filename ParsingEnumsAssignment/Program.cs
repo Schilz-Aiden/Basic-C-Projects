3cf564780a38c7c78d2a4ebbba829d151f97d9bf
Schilz-Aiden <aidenschilz5@gmail.com>
2025-08-08 11:25:21 -0600

    Add files via upload

---
+++ ParsingEnumsAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    // enum for days of the week
+    enum DaysOfWeek
+    {
+        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ask user for the current day
+            Console.Write("Enter the current day of the week: ");
+            string userInput = Console.ReadLine(); // read what they typed
+
+            try
+            {
+                // try to convert the string to the enum type
+                DaysOfWeek currentDay = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), userInput, true);
+
+                // show the day back to them
+                Console.WriteLine("You picked: " + currentDay);
+            }
+            catch
+            {
+                // if parsing failed, tell them they typed it wrong
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            // keep console open
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ParsingEnumsAssignment/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ParsingEnumsAssignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ParsingEnumsAssignment")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("68755b9a-8b09-467a-8733-3eea6953e051")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
